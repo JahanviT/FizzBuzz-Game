72832af6e04139f9102bf03f6ba23e84f99d02cb
Jahanvi Thakkar <82188673+JahanviT@users.noreply.github.com>
2022-09-14 01:38:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+var number = prompt ("Enter a number: ")
+if (number % 3 === 0 && number % 5 === 0){
+alert("FizzBuzz");
+}
+else if (number % 3 === 0){
+  alert("fizz");
+}
+else if (number % 5 === 0){
+  alert("Buzz");
+}
+else {
+  alert(number);
+}
+
+
+
+
+var number = 1;
+
+
+
+
+
+
+
+// var number = promt("Enter a number:")
+// function fizzBuzz() {
+// if (number % 3 === 0 && number % 5 === 0){
+//   output.push("FizzBuzz");
+// }
+// else if (number % 3 === 0){
+//   output.push("fizz");
+// }
+// else if (number % 5 === 0){
+//   output.push("Buzz");
+// }
+// else {
+//   output.push(number);
+// }
+//   output.push(number);
+//   number++;
+//  alert(output);
+// }
+// var output = [];
+// var number = 1;
